bd3876c7efbdb6c03948a9a359e705386d3f3115
av993 <ameyav993@gmail.com>
2021-10-03 23:33:13 -0400

    TEst

--- stra.c
+++ stra.c
@@ -80,7 +80,6 @@ int Str_compare(const char s1[], const char s2[]) {
         } else if (s2[i] == '\0' && s1[i] != '\0') {
             return -1;
         }
-
         if (s1[i] < s2[i]) {
             return -1;
         } else if (s1[i] < s2[i]) {
