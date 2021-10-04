1047fff568df22312fae7bba3e79540b08157568
av993 <ameyav993@gmail.com>
2021-10-04 01:32:04 -0400

    Fixed another bug

--- stra.c
+++ stra.c
@@ -132,13 +132,3 @@ char *Str_search(const char *haystack, const char *needle) {
 
 }
 
-int main() {
-      const char acHaystack[] = "abcde";
-      const char acNeedle[] = "bcdef";
-    char* pcResult1 = Str_search(acHaystack, acNeedle);
-    char* pcResult2 = strstr(acHaystack, acNeedle);
-    printf("%s\n", pcResult1);
-    printf("%s\n", pcResult2);
-    return 0;
-   
-}
