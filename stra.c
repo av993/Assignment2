78a23cadff9cf4282cacefc6973d5b2d1b6e1ff0
av993 <ameyav993@gmail.com>
2021-10-05 15:04:26 -0400

    Updated

--- stra.c
+++ stra.c
@@ -76,6 +76,8 @@ int Str_compare(const char s1[], const char s2[]) {
     length1 = Str_getLength(s1);
     length2 = Str_getLength(s2);
 
+    /* This attempts to find the longest length between 
+    the strings s1 and s2 and set equal to max_length*/
     if (length1 < length2) {
         max_length = length2;
     } else {
@@ -83,6 +85,11 @@ int Str_compare(const char s1[], const char s2[]) {
     }
 
     for (i = 0; i < max_length; i++) {
+
+        /* If both have null bytes at the
+        same point then they are the same and 
+        if s1 ends before s2, then return -1 
+        and vice versa */
         if (s1[i] == '\0') {
             if (s2[i] == '\0') {
                 return 0;
@@ -92,6 +99,10 @@ int Str_compare(const char s1[], const char s2[]) {
         } else if (s2[i] == '\0') {
             return 1;
         }
+
+        /* If the character in s1 
+        is smaller than the character in s2, then
+        return -1 and vice versa */
 
         if (s1[i] < s2[i]) {
             return -1;
@@ -125,12 +136,28 @@ char *Str_search(const char *pcHaystack, const char *pcNeedle) {
         return (char*)pcHaystack;
     }
 
+    /*
+        This outer loop runs for each character until the
+        end of pcHaystack.
+    */
     while (pcHaystack[i] != '\0') {
         j = i;
         pcNeedleStart = -1;
         pcNeedleIndex = 0;
 
+        /*
+            This inner loop runs for every character of pcHaystack
+            in order to find where the Str_search algorithmn ends
+        */
         while (pcHaystack[j] != '\0') {
+
+            /*
+                If characters of pcNeedle match characters of
+                pcHaystack, then indicate where the 
+                match was found and continue looping so that you can 
+                find whether the next couple of characters match the 
+                needle string.
+            */
             if (pcHaystack[j] == pcNeedle[pcNeedleIndex]) {
                 if (pcNeedleIndex == 0) {
                     pcNeedleStart = j;
@@ -143,6 +170,10 @@ char *Str_search(const char *pcHaystack, const char *pcNeedle) {
             j++;
         }
         
+        /*
+            If the needle was found, then return an
+            address to the beggining of the needle in the haystack.
+        */
         if (pcNeedleIndex == pcNeedleLength) {
             return (char*)&pcHaystack[pcNeedleStart];
         }
--- strp.c
+++ strp.c
@@ -80,6 +80,8 @@ int Str_compare(const char s1[], const char s2[]) {
     length1 = Str_getLength(s1);
     length2 = Str_getLength(s2);
 
+    /* This attempts to find the longest length between 
+    the strings s1 and s2 and set equal to max_length*/
     if (length1 < length2) {
         max_length = length2;
     } else {
@@ -90,6 +92,12 @@ int Str_compare(const char s1[], const char s2[]) {
     s2Ptr = s2;
 
     for (i = 0; i < max_length; i++) {
+    
+        /* If both have null bytes at the
+        same point then they are the same and 
+        if s1 ends before s2, then return -1 
+        and vice versa */
+
         if (*s1Ptr == '\0') {
             if (*s2Ptr  == '\0') {
                 return 0;
@@ -100,6 +108,9 @@ int Str_compare(const char s1[], const char s2[]) {
             return 1;
         }
 
+        /* If the character in s1 
+        is smaller than the character in s2, then
+        return -1 and vice versa */
         if (*s1Ptr < *s2Ptr) {
             return -1;
         } else if (*s1Ptr > *s2Ptr) {
@@ -135,12 +146,28 @@ char *Str_search(const char *pcHaystack, const char *pcNeedle) {
     needlePtr = pcNeedle;
     needleStart = NULL;
 
+    /*
+        This outer loop runs for each character until the
+        end of pcHaystack.
+    */
     while (*haystackPtr != '\0') {
         innerHaystackPtr = haystackPtr;
         needleStart = NULL;
         needlePtr = pcNeedle;
 
+        /*
+            This inner loop runs for every character of pcHaystack
+            in order to find where the Str_search algorithmn ends
+        */
         while (*innerHaystackPtr != '\0') {
+
+            /*
+                If characters of pcNeedle match characters of
+                pcHaystack, then indicate where the 
+                match was found and continue looping so that you can 
+                find whether the next couple of characters match the 
+                needle string.
+            */
             if (*innerHaystackPtr == *needlePtr) {
                 if (needlePtr == pcNeedle) {
                     needleStart = innerHaystackPtr;
@@ -153,6 +180,10 @@ char *Str_search(const char *pcHaystack, const char *pcNeedle) {
             innerHaystackPtr++;
         }
         
+        /*
+            If the needle was found, then return an
+            address to the beggining of the needle in the haystack.
+        */
         if (*needlePtr == '\0') {
             return (char*)needleStart;
         }
